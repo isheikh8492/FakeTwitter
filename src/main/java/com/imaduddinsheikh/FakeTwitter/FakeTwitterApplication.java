7b1b72c976cc4bd11976412f00bfd6dbc356fba8
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-07-12 18:11:35 -0500

    Initial commit

---
+++ src/main/java/com/imaduddinsheikh/FakeTwitter/FakeTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.imaduddinsheikh.FakeTwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeTwitterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imaduddinsheikh/FakeTwitter/FakeTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imaduddinsheikh.FakeTwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
